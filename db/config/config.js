f6663b34f8682857d2c12af1fa0c2a8f57327866
florensius daniel josef <capricondaniel@gmail.com>
2022-12-08 20:30:43 +0700

    production setting

--- db/config/config.js
+++ db/config/config.js
@@ -5,14 +5,14 @@ module.exports = {
 		username: process.env.DB_USERNAME_DEV || "postgres",
 		password: process.env.DB_PASSWORD_DEV || "123456",
 		database: process.env.DB_DEV || "database_development",
-		host: "127.0.0.1",
+		host: process.env.DB_HOST,
 		dialect: "postgres",
 	},
 	production: {
 		username: process.env.DB_USERNAME_PROD || "postgres",
 		password: process.env.DB_PASSWORD_PROD || "123456",
 		database: process.env.DB_PROD || "database_production",
-		host: "127.0.0.1",
+		host: process.env.DB_HOST,
 		dialect: "postgres",
 	},
 };
--- db/models/connect.js
+++ db/models/connect.js
@@ -1,13 +1,14 @@
 const { Sequelize } = require("sequelize");
 
 const connect = new Sequelize(
-	process.env.DB_DEV,
-	process.env.DB_USERNAME_DEV,
-	process.env.DB_PASSWORD_DEV,
-	{
-		host: "localhost",
-		dialect: "postgres",
-	}
+	// process.env.DB_DEV,
+	// process.env.DB_USERNAME_DEV,
+	// process.env.DB_PASSWORD_DEV,
+	// {
+	// 	host: "localhost",
+	// 	dialect: "postgres",
+	// }
+	process.env.DATABASE_URL
 );
 
 export default connect;
