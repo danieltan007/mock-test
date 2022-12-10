4367bc9edd31670f5af4c320b76c78cf5e280115
florensius daniel josef <capricondaniel@gmail.com>
2022-12-10 16:18:38 +0700

    perbaiki connect database logic

--- db/models/connect.js
+++ db/models/connect.js
@@ -1,19 +1,16 @@
 const { Sequelize } = require("sequelize");
 
-let connect = "";
-
-if (process.env.NODE_ENV === "development") {
-	connect = new Sequelize(
-		process.env.DB_DEV,
-		process.env.DB_USERNAME_DEV,
-		process.env.DB_PASSWORD_DEV,
-		{
-			host: "localhost",
-			dialect: "postgres",
-		},
-	);
-} else {
-	connect = new Sequelize(process.env.DATABASE_URL);
-}
+const connect =
+	process.env.NODE_ENV === "development"
+		? new Sequelize(
+				process.env.DB_DEV,
+				process.env.DB_USERNAME_DEV,
+				process.env.DB_PASSWORD_DEV,
+				{
+					host: "localhost",
+					dialect: "postgres",
+				},
+		  )
+		: new Sequelize(process.env.DATABASE_URL);
 
 export default connect;
